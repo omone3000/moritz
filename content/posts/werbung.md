e23455c578c4b33a110ec166440fbe6dfcc8f2fb
Frank Matuse <frank.matuse@nueva.ch>
2020-04-19 13:13:30 +0200

    Initial Commit

---
+++ content/posts/werbung.md
@@ -0,0 +1,42 @@
+---
+slug: werbung
+title: Beispiele zu meinem Werbeportfolio
+date: 2020-04-18
+published: true
+tags: ["werbung", "homeschooling"]
+series: false
+cover_image: ./images/werbeportfolio.jpg
+canonical_url: false
+description: "Während des Home-Schoolings habe ich ein Werbeportfolio angefertigt. Hierbei habe ich auch 4 verschiedene Werbungen etwas genauer untersucht. Die Videos und Bilder dazu habe ich in diesem Artikel zusammengefasst."
+---
+
+Hier habe ich die Beispiele zur Untersuchung 4 verschiedener Werbungen aufgelistet:
+
+### 1. Sixt «Überflieger»
+
+Flughafen Berlin-Tegel, Fluggastbrücke Gate A8
+
+![Sixt Flieger](./images/sixt-flieger.jpg)
+
+![Sixt Flieger](./images/sixt-ueberflieger.jpg)
+
+### 2. Nintendo Switch
+Direkt zu <a href="https://www.youtube.com/watch?v=jqPuykfbNg8" target="_blank">YouTube</a>.
+<div class="video-container"><iframe width="560" height="315" src="https://www.youtube.com/embed/jqPuykfbNg8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+
+### 3. Haribo TV Spots
+
+<div class="video-container"><iframe width="560" height="315" src="https://www.youtube.com/embed/_AuXHidxbec" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+
+<div class="video-container">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/U90HzBnriqE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+
+### 4. Ritter Sport
+
+![Ritter Sport Currywurst](https://s3-eu-central-1.amazonaws.com/rs-blog-media-prod/wp-content/uploads/2017/10/04154058/170926_RI_Blogvoting_Berlin_1.png)
+
+Quelle: <a href="https://www.ritter-sport.de/blog/2017/10/04/deutschland-deine-ritter-sport-plakate/" target="_blank">Ritter Sport Blog</a>
+
+
+
---
+++ src/templates/Post.vue
@@ -0,0 +1,149 @@
+<template>
+  <Layout>
+    <div class="post-title">
+      <h1 class="post-title__text">
+        {{ $page.post.title }}
+      </h1>
+
+      <PostMeta :post="$page.post" />
+
+    </div>
+
+    <div class="post content-box">
+      <!--
+      <div class="post__header">
+        <g-image alt="Cover image" v-if="$page.post.cover_image" :src="$page.post.cover_image" />
+      </div>
+      -->
+      <div class="post__content" v-html="$page.post.content" />
+
+      <div class="post__footer">
+        <PostTags :post="$page.post" />
+      </div>
+    </div>
+
+    <div class="post-comments">
+      <!-- Add comment widgets here -->
+    </div>
+
+    <Author class="post-author" />
+  </Layout>
+</template>
+
+<script>
+import PostMeta from '~/components/PostMeta'
+import PostTags from '~/components/PostTags'
+import Author from '~/components/Author.vue'
+
+export default {
+  components: {
+    Author,
+    PostMeta,
+    PostTags
+  },
+  metaInfo () {
+    return {
+      title: this.$page.post.title,
+      meta: [
+        {
+          name: 'description',
+          content: this.$page.post.description
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<page-query>
+query Post ($id: ID!) {
+  post: post (id: $id) {
+    title
+    path
+    date (format: "D. MMMM YYYY")
+    timeToRead
+    tags {
+      id
+      title
+      path
+    }
+    description
+    content
+    cover_image (width: 860, blur: 10)
+  }
+}
+</page-query>
+
+<style lang="scss">
+.post-title {
+  padding: calc(var(--space) / 2) 0 calc(var(--space) / 2);
+  text-align: center;
+}
+
+.post {
+
+  &__header {
+    width: calc(100% + var(--space) * 2);
+    margin-left: calc(var(--space) * -1);
+    margin-top: calc(var(--space) * -1);
+    margin-bottom: calc(var(--space) / 2);
+    overflow: hidden;
+    border-radius: var(--radius) var(--radius) 0 0;
+
+    img {
+      width: 100%;
+    }
+
+    &:empty {
+      display: none;
+    }
+  }
+
+  &__content {
+    h2:first-child {
+      margin-top: 0;
+    }
+
+    p:first-of-type {
+      font-size: 1.2em;
+      color: var(--title-color);
+    }
+
+    img {
+      display: block;
+      max-width: 100%;
+    }
+  }
+}
+
+.post-comments {
+  padding: calc(var(--space) / 2);
+
+  &:empty {
+    display: none;
+  }
+}
+
+.post-author {
+  margin-top: calc(var(--space) / 2);
+}
+
+.video-container {
+    position: relative;
+    padding-bottom: 56.25%;
+    padding-top: 0;
+    height: 0;
+    overflow: hidden;
+    margin-bottom: 2em
+}
+
+.video-container iframe,
+.video-container object,
+.video-container embed {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+</style>
